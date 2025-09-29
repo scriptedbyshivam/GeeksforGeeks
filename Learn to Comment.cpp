70da6f07143a49e7649d5433130d6ece22258849
Shivam-Maurya-2024 <shivam2024maurya@gmail.com>
2025-09-29 14:09:39 +0530

    Problem 4

---
+++ Learn to Comment.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void printValues(int a, int b, int c) {
+        // code here
+        
+        cout << a << " ";
+        // cout << b << " ";
+        //Only the value of a and c is printed as the line of code that prints b is commented.
+        cout << c << " ";
+    }
+};
