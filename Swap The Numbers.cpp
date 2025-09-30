c9c1f10f53faf22e7fc5b219f94aa215f9017577
Shivam-Maurya-2024 <shivam2024maurya@gmail.com>
2025-09-30 21:56:02 +0530

    Swap THe Number

---
+++ Swap The Numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int d;
+    d=a;
+    a=b;
+    b=d;
+    // Write Code to Swap
+
+    cout << a << " " << b << endl;
+    return 0;
+}
