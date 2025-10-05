8b6c130a18696fbe27e2f67cd145ae260f0bcf9e
Shivam-Maurya-2024 <shivam2024maurya@gmail.com>
2025-10-05 10:17:57 +0530

    Taking input

---
+++ Taking input.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void inputData() {
+        // code here
+        int a;
+        string b;
+        cin>>a;
+        cin>>b;
+        
+        cout<<a<<" "<<b<<endl;
+        
+    }
+};
