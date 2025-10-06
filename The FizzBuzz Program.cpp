032fcc6eea215a7f8b2dcf2b7ab6e25bbc4a203d
Shivam-Maurya-2024 <shivam2024maurya@gmail.com>
2025-10-06 18:48:26 +0530

    The FizzBuzz Program.cpp

--- The FizzBuzz Program.cpp
+++ The FizzBuzz Program.cpp
@@ -1,25 +1,54 @@
-void fizzBuzz(int number) {
-     if(number % 3 == 0 && number % 5 == 0){
-    cout<<"FizzBuzz"<<endl;
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int i;
+    cin>>i;
+    if(i%3==0 && i%5==0){
+        cout<<"FizzBuzz";
+    }
+    else if(i%3==0){
+        cout<<"Fizz";
+    }
+    else if(i%5==0){
+        cout<<"Buzz";
+    }
+    else{
+        cout<<i;
+    }
+    return 0;
+}
+
+
+
+
+
+// void fizzBuzz(int number) {
+
+
+//      if(number % 3 == 0 && number % 5 == 0){
+//     cout<<"FizzBuzz"<<endl;
 
     
-}
+// }
 
-else if(number % 3 == 0 )
-{
-    cout<<"Fizz"<<endl;
-}
+// else if(number % 3 == 0 )
+// {
+//     cout<<"Fizz"<<endl;
+// }
 
 
-else if(number % 5 == 0 )
-{
-    cout<<"Buzz"<<endl;
-}
+// else if(number % 5 == 0 )
+// {
+//     cout<<"Buzz"<<endl;
+// }
 
  
 
-else{
-    cout<<number<<endl;
-}
+// else{
+//     cout<<number<<endl;
+// }
 
-}
+// }
+
