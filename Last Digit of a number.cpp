896d7a30173236511ada203bf12e6cf5f5d71880
Shivam-Maurya-2024 <shivam2024maurya@gmail.com>
2025-10-07 18:29:38 +0530

    Last Digit of a number

---
+++ Last Digit of a number.cpp
@@ -0,0 +1,17 @@
+void utility(int n) {
+
+    // just complete below statement
+    int ans = abs(n)%10;
+    
+    // abs() or absolute value
+    // Returns the positive version of any number.
+
+    
+    
+    
+    // 1234 divided by 10 = 123 remainder **4**
+    // so the last number of digit is 4
+
+        // print the result
+        cout << ans << endl;
+}
