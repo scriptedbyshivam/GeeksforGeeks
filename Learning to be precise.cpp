d9201fc9503855b3fdf0d70b4d1744f86bf8d96d
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-10 20:08:10 +0530

    Learning to be precise

---
+++ Learning to be precise.cpp
@@ -0,0 +1,10 @@
+void precise(float a, float b) {
+    // perform a/b
+
+double c;
+c =a/b;
+    cout <<c<<" "<<fixed<<setprecision(3)<<c<<endl;
+    
+ // to use setpresicion we have mention frist their library include<iomanip> 
+ //also remember the <<fixed<<
+}
