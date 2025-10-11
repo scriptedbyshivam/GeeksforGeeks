46d965d3ed98567f6d7565becec2347651ca35f2
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-11 21:59:25 +0530

    Prime Factorization

--- Prime Factorization.cpp
+++ Prime Factorization.cpp
@@ -13,7 +13,7 @@ void printPrimeFactorization(int n) {
         }
     }
 }
-/*Step	
+/*
 n <= 1	Chhoti value ka factorization nahi hota
 for (i = 2; i <= n; i++)	Har number se divide karna check karte hain
 if (n % i == 0)	Agar divide hota hai, toh wo factor hai
@@ -40,4 +40,13 @@ i = 1	Wapas 2 se shuru karte hain, same factor baar baar pakadne ke liye*/
 
 
 //yaha pr int i for loop
-//while se validity check karna  tha
+//yaha par while loop tab tak chalta rahega jab tak iski condition false nahi ho jati
+//example:- 100 
+/* 100%2==0
+    phir n=n/i;
+again while loop chalega 50%2==0
+    phir n=n/i;
+agin while loop condition cheack hoga ,25%2!=0;
+condition false, now go to for loop again and ab i++ hoga then 
+ab i=3 par ayse hi condition check hogi*/
+
