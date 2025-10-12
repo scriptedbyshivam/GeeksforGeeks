73b6191bdabfcadc3c3caf0ef641811f69029448
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-12 19:25:18 +0530

    The If Statement

--- The If Statement.cpp
+++ The If Statement.cpp
@@ -1,10 +1,31 @@
-// User function Template for C++
+#include <iostream>
+using namespace std;
 
-void utility(int number) {
-    // Write your if statement below
-if(number>100){
-    cout<<"Big"<<endl;
+int main() {
+    // code here
+    int n;
+    cin>>n;
+    if(n>100){
+        cout<<"Big"<<endl;
+    }
+    
+    cout<<"Number";
+
+    return 0;
 }
-    // Write your if statement above
-    cout << "Number" << endl;
-}
+
+//ANOTHER CODE
+
+
+// // User function Template for C++
+
+// void utility(int number) {
+//     // Write your if statement below
+// if(number>100){
+//     cout<<"Big"<<endl;
+// }
+//     // Write your if statement above
+//     cout << "Number" << endl;
+// }
+
+
