0fdd482761348fc76b0b995eaab9ec634cb54f52
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-12 19:34:55 +0530

    C++ Strings

---
+++ C++ Strings.cpp
@@ -0,0 +1,13 @@
+string conCat(string s1, string s2) {
+    // code here.
+    return s1 +s2;
+    //to return both string s1 and s2 we use +   ;
+//     Example 1:
+
+// Input:
+// S1 = "Geeksfor"
+// S2 = "Geeks"
+// Output: GeeksforGeeks
+// Explanation: Combined "Geeksfor" and "Geeks"
+}
+
