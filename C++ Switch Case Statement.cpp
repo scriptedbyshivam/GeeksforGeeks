a97073694c925fdfa19c2bfb8b887671cd877269
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-13 21:27:35 +0530

    C++ Switch Case Statement

---
+++ C++ Switch Case Statement.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  public:
+    void isInRange(int n) {
+        // you don't need to take input
+        // here just write code which print correct answer according to n
+        switch(n){
+            case 1:
+            cout<<"one"<<endl;
+            break;
+            case 2:
+            cout<< "two"<<endl;
+            break;
+            case 3: 
+            cout<< "three"<<endl;
+            break;
+            case 4: 
+            cout<< "four"<<endl;
+            break;
+            case 5:
+            cout<<"five"<<endl;
+            break;
+            case 6:
+            cout<<"six"<<endl;
+            break;
+            case 7:
+            cout<<"seven"<<endl;
+            break;
+            case 8:
+            cout<<"eight"<<endl;
+            break;
+            case 9:
+            cout<<"nine"<<endl;
+            break;
+            case 10:
+            cout<<"ten"<<endl;
+            break;
+            
+            default: cout<<"not in range"<<endl;
+        }
+    }
+};
