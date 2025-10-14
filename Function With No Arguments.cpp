ac926da4544b272bde8e2b0c2042dfb108cc7e89
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-14 19:46:19 +0530

    Function With No Arguments

---
+++ Function With No Arguments.cpp
@@ -0,0 +1,25 @@
+// User function Template for C++
+
+// Write the helloFunction below. It should have one statement
+// cout<<"Hello"<<endl;
+//  code here
+void helloFunction(){
+    cout<<"Hello"<<endl;
+
+}
+
+/* Working Flow OF a Program
+void helloFunction(){ ... }
+Ye ek function definition hai jiska naam hai helloFunction.
+Is function ka return type void hai, matlab ye kuch return nahi karega.
+Function ke andar sirf ek hi statement hai:
+cout << "Hello" << endl;
+yeh line Output Karne Ke Liye H
+with no input When We call the function The Hello should be Print  On Screen*/
+
+
+
+// int main(){
+//     helloFunction();
+//     return 0;
+// }
