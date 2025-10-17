54b772bccc555bf111ead7cf759d6d6e65411368
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-17 18:30:34 +0530

    Print With Separator

---
+++ Print With Separator.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+void utility() {
+    string a, b, separator;
+    cin >> a >> b >> separator;
+    cout<<a<<separator<<b<<endl;
+    
+    // Your code here
+    
+}
