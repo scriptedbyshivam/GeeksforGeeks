74f38f5bbc3af5aee9f8b142848a6311d995f77a
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-19 15:13:19 +0530

    C++ Functions (Sum of numbers) | Set 1

---
+++ C++ Functions (Sum of numbers) Set 1.cpp
@@ -0,0 +1,12 @@
+/*  Write your function here */
+
+/* The function should be named calcSum
+    and accepts three parameters of integer type
+    and returns the sum of the three integers */
+
+int calcSum(int a, int b, int c) {
+    // Your code here
+    int d;
+    d=a+b+c;
+    return d;
+}
