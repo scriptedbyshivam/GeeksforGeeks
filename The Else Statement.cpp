d8bb2909fe40a6d657ec413508ab5b6f1a9ae38b
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-21 23:46:29 +0530

    The Else Statement
    Added comments and an alternate solution template.

--- The Else Statement.cpp
+++ The Else Statement.cpp
@@ -13,3 +13,25 @@ int main() {
 
     return 0;
 }
+
+
+
+/*
+alternate way to solve (this thing use in new question)
+
+// User function Template for C++
+
+void utility(int number) {
+    if(number>100){
+        cout<< "Big"<<endl;
+    }
+    else{
+        cout<< "Number"<<endl;
+    }
+
+    // Write your if-else statement below
+
+    // Write your if-else statement above
+}
+
+*/
