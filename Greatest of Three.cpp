8a9d3cbfb5b1047f2838e98a3ab55fdbf72313d4
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-25 21:47:06 +0530

    Greatest of Three

---
+++ Greatest of Three.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a>b&&a>c){
+        cout<<a;
+    }
+    else if(b>a&&b>c){
+        cout<<b;
+    }
+    else{
+        cout<<c;
+    }
+
+    return 0;
+}
+/*
+&& mai dono hi condtion sahi hona chahihye tabhi vo loop aage work karega
+|| mai agar ek bhi condition sahi hui toh vo loop ko work kar dega
