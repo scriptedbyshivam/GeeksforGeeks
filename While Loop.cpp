fe3f7e3ad6300e215e15c027ad45b3881733fd7b
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-25 21:13:56 +0530

    While Loop

--- While Loop.cpp
+++ While Loop.cpp
@@ -1,8 +1,31 @@
-void utility(int x) {
-    // code here
-    while(x>=0){
-        // while (x >= 0) ? loop continues until x reach to 0.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    while(x!=-1){
         cout<<x<<" ";
         x--;
     }
+    // use while loop over x
+    // to print integers from x to 0
+
+    return 0;
 }
+
+
+
+
+
+// alernate way 
+
+// void utility(int x) {
+//     // code here
+//     while(x>=0){
+//         // while (x >= 0) ? loop continues until x reach to 0.
+//         cout<<x<<" ";
+//         x--;
+//     }
+// }
+
