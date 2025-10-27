cdf1c98d11976eb1a090465a66229e5222fe3434
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-27 22:51:19 +0530

    The Else if Statement

---
+++ The Else if Statement.cpp
@@ -0,0 +1,14 @@
+void utility(int number) {
+    // Write the if, else if, else statements below
+    if(number<10){
+        cout<<"Small"<<endl;
+    }
+    else if(10<number&&number<100){
+        cout<<"Number"<<endl;
+        
+    }
+    else if(number>100){
+        cout<<"Big"<<endl;
+    }
+}
+/*Remember the difference b/w &&,||   */
