3783237892c447b46a7fcd5e412731e825648e9d
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-29 20:51:34 +0530

    Gr

--- Greatest of Three.cpp
+++ Greatest of Three.cpp
@@ -1,24 +1,61 @@
-#include <iostream>
-using namespace std;
+// User function Template for C++
 
-int main() {
-    // code here
-    int a,b,c;
-    cin>>a>>b>>c;
-    if(a>b&&a>c){
-        cout<<a;
+int find_greatest_number(int a, int b, int c) {
+    // Write your code here..
+    if(a>=b && a>=c){
+        return a;
     }
-    else if(b>a&&b>c){
-        cout<<b;
+    else if(b>=a && b>=c){
+        return b;
     }
-    else{
-        cout<<c;
+    else {
+        return c;
     }
-
-    return 0;
 }
+// remember the >= to condtion 
+// example: input: 10
+//                 10
+//                 4
+
+
+
+
+
+
+//alternate way but prefer above one
+
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     // code here
+//     int a,b,c;
+//     cin>>a>>b>>c;
+//     if(a>b&&a>c){
+//         cout<<a;
+//     }
+//     else if(b>a&&b>c){
+//         cout<<b;
+//     }
+//     else{
+//         cout<<c;
+//     }
+
+//     return 0;
+// }
+
+
+
 /*
 && mai dono hi condtion sahi hona chahihye tabhi vo loop aage work karega
 || mai agar ek bhi condition sahi hui toh vo loop ko work kar dega
 */
+
 
