582f1284bf77ad614e4055d22edcd8e0c2c23eb0
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-29 20:40:54 +0530

    Array Traversal

---
+++ Array Traversal.cpp
@@ -0,0 +1,9 @@
+void arrayTraversal(int numbers[], int size) {
+    // Code here
+    for(int i=0;i<size;i++){
+        cout<<numbers[i]<<" ";
+        // return numbers[i] " ";
+    }
+}
+//Meaning:- 
+// Array Traversal means visiting each element of an array one by one —usually from the first element to the last element — to perform some operation (like printing, searching, or updating values).
