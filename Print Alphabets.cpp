3b8a7b49447f8fe3de6b9ae86b1d0c64eae9a896
Shivam Maurya <shivam2024maurya@gmail.com>
2025-10-31 20:41:02 +0530

    Print Alphabets

---
+++ Print Alphabets.cpp
@@ -0,0 +1,16 @@
+// User function Template for C++
+
+// function should print the alphabets
+// from c1 to c2 (space separated)
+// No need to print the new line
+// The new line will be printed by the
+// driver's code.
+
+void alphabets(char c1, char c2) {
+    // code here
+    for(char i=c1; i<=c2;i++){
+        cout<<i<<" ";
+    }
+}
+
+
