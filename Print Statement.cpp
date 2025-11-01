31adac3b129abd4e44bdb4ed90d8f11d77ac1e8c
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-01 19:12:22 +0530

    Print Statement

---
+++ Print Statement.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void utility() {
+        string s;
+        getline(cin,s);
+        cout<<s<<endl;
+    }
+};
+
+//Here remember the uses of getline 
+// getline use to neglect the space between words
+// example :-uses for Writing full name with spaces
