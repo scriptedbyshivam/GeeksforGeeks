8721a4277cde7485d75f04f3db87513e33f5038c
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-04 22:44:57 +0530

    Slice The String

---
+++ Slice The String.cpp
@@ -0,0 +1,25 @@
+// User function Template for C++
+
+// functions should slice the given string
+// i.e. remove the first and the last character
+// of the given string and return the sliced
+// string
+string sliceString(string &s) {
+    // code here
+    int a=s.size();
+    string b;
+    for(int i=1;i<a-1;i++){
+        b+=s[i];
+        
+        
+        
+    }
+    return b;
+    
+}
+/*
+
+remember the  string b; bahar aayega loop ke
+and return b bhi loop ke bahar hoga.
+
+*/
