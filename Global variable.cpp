c89622c7692a81ee0695bceb652543920bde5875
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-04 22:28:54 +0530

    Global variable

---
+++ Global variable.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// declare a global variable g
+    int g=10;
+    
+int main() {
+
+    // printing the value of global variable g
+    cout << g << endl;
+
+    return 0;
+}
+/*
+
+int g = 10;
+
+Yaha main() function ke bahar likhi gayi hai, isliye g global variable hai.
+Global variable ka matlab hai — program ke kisi bhi function (main(),
+ya koi aur function) se access kar sakte ho.
+*/
