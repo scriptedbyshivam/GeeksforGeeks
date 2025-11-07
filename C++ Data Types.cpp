fc0fc84ca037b62b4f097ca55ffe90433ded9e4e
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-07 19:49:47 +0530

    C++ Data Types

---
+++ C++ Data Types.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int cppIntType() {
+        // code here
+        int i;
+        cin>>i;
+        return i;
+        
+    }
+
+    char cppCharType() {
+        char ch;
+        cin>>ch;
+        return ch;
+        
+        // code here
+    }
+
+        
+    float cppFloatType() {
+        // code here
+        float f;
+        cin>>f;
+        return f;
+        
+    }
+};
