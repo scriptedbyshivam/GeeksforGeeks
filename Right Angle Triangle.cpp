bf17b13fffa49947163d3fc754777f1e70c9a1cf
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-08 23:22:00 +0530

    Right Angle Triangle

---
+++ Right Angle Triangle.cpp
@@ -0,0 +1,13 @@
+// User function Template for C++
+
+void triangleWall(int s) {
+    for (int i=0;i<s;i++){
+        
+        for(int j=0;j<=i;j++){
+            cout<<"*"<<" ";
+        }cout<<endl;
+    }
+
+    // Write your code here
+    
+}
