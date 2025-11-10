cd8b01b00c968c221a69781ea99db472c30f241e
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-10 11:19:58 +0530

    Print GeeksForGeeks with quotes

---
+++ Print GeeksForGeeks with quotes.cpp
@@ -0,0 +1,25 @@
+// User function Template for C++
+
+void print() {
+
+    // code here
+    cout<<"\"GeeksForGeeks\"";
+}
+
+/*
+We use a backslash (\) before GeeksForGeeks
+this is called escape character.
+
+Iska matlab hai agla character ko normal treat mat karo,balki special symbol samjho.
+
+
+
+\"         --->  	Double quote
+\'         --->	    Single quote	
+\\         --->	    Backslash	
+\n         ---> 	New line	
+\t         --->   	Tab space
+
+
+
+*/
