061171d0de47638c27660478b0e51f571a8f5e81
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-11 18:02:08 +0530

    Switch Statement

--- Switch Statement.cpp
+++ Switch Statement.cpp
@@ -45,3 +45,61 @@ string utility(int number) {
         
     }
 }
+
+
+
+
+
+
+
+
+
+// this is alternate problem solution of same topic
+
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int a;
+    cin>>a;
+    
+    switch(a){
+        case 1:cout<<"One";
+        break;
+        
+        case 2:cout<<"Two";
+        break;
+        
+        case 3:cout<<"Three";
+        break;
+        
+        case 4:cout<<"Four";
+        break;
+        
+        case 5:cout<<"Five";
+        break;
+        
+        case 6:cout<<"Six";
+        break;
+        
+        case 7: cout<<"Seven";
+        break;
+        
+        case 8: cout <<"Eight";
+        break;
+        
+        case 9:cout<<"Nine";
+        break;
+        
+        default: cout<<"Unknown";
+    }
+
+    // using switch statement
+
+    return 0;
+}
+
+*/
