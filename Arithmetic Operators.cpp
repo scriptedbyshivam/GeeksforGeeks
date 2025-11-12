7420424125a7e0355d9e9d8f31608264ec82af95
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-12 22:33:26 +0530

    Arithmetic Operators

--- Arithmetic Operators.cpp
+++ Arithmetic Operators.cpp
@@ -10,3 +10,30 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+//alternate solution for alertnate problem but topic is same
+
+
+// User function Template for C++
+
+// void operations(int x, int y) {
+//     // Perform addition x+y below
+//     int p =x+y;
+//         // Perform subtraction x-y below
+//         int q =x-y;
+//             // Perform multiplication x*y below
+//         int r =x*y;
+//             // Perform division x/y below
+//         int s =x/y;
+//             // Perform modulo operation x%y below
+//         int t =x%y;
+
+//             // The below code prints the output. Don't change it!
+//         cout << p << " " << q << " " << r << " " << s << " " << t << endl;
+// }
+
