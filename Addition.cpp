0667bc8706523e522bf4d1c38d1e07bb9bf3e3f1
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-12 23:16:54 +0530

    Addition

---
+++ Addition.cpp
@@ -0,0 +1,18 @@
+// User function Template for C++
+
+// create a class addition
+// with a function add
+// this should return the sum of
+// two values(a,b) passed in the parameter
+
+// code here
+class Addition{
+    public:
+    
+    int add(int a,int b){
+        int sum =a+b;
+        return sum;
+        
+    }
+};
+
