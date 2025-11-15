9fc3825cace1859e010a2ebb6b4bb20d99a21b30
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-15 22:13:22 +0530

    Inverted Right AngleTriangle

---
+++ Inverted Right AngleTriangle.cpp
@@ -0,0 +1,24 @@
+// User function Template for C++
+
+void invTriangleWall(int s) {
+
+    // Write your code here
+    for(int i=s;i>0;i--){
+       
+        
+        for(int j=0;j<i;j++){
+             cout<<"* ";
+         
+        } cout <<endl;
+            
+            
+    }
+            
+            
+}
+
+
+/*
+remember how this loop working and syntax (>)
+for(int i=s;i>0;i--)
+*/
