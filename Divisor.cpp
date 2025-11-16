48e5d112971d937786e5c2b59ee690a2300d5c6c
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-16 21:11:29 +0530

    Divisor

--- Divisor.cpp
+++ Divisor.cpp
@@ -10,3 +10,37 @@ void divisor(int n) {
     // Write your code here
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alternate solution for alternate problem but topic is same 
+
+
+
+
+
+/*
+
+class Solution {
+  public:
+    void printDivisors(int n) {
+        // code here
+        for(int i=1;i<1000;i++){
+            if(n%i==0){
+                cout<<i<<" ";
+            }
+        }
+    }
+};
+
+*/
