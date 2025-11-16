cb7ba473b277af44b1ff61d604e2fbb66f056617
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-16 21:18:20 +0530

    Inverted Right AngleTriangle

--- Inverted Right AngleTriangle.cpp
+++ Inverted Right AngleTriangle.cpp
@@ -22,3 +22,32 @@ void invTriangleWall(int s) {
 remember how this loop working and syntax (>)
 for(int i=s;i>0;i--)
 */
+
+
+
+
+
+
+
+
+
+//alternate solution for alternate problem but topic is same 
+
+
+/*
+
+class Solution {
+  public:
+    void printPattern(int n) {
+        // code here
+        for(int i=n;i>0;i--){
+            for(int j=1;j<=i;j++)
+            {
+                cout<<"*"<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+*/
