7679bf8539d1ffd5fa50e8af65e4e34509a00d29
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-17 21:49:28 +0530

    Right Angle Triangle

--- Right Angle Triangle.cpp
+++ Right Angle Triangle.cpp
@@ -11,3 +11,29 @@ void triangleWall(int s) {
     // Write your code here
     
 }
+
+
+
+//alternate solution for alternate problem but topic is same 
+
+/*
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    // print right angle triangle of size n
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }cout<<endl;
+    }
+
+    return 0;
+}
+
+
+
+*/
