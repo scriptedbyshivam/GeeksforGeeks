1642bc983bc8eb296fe732181f133c5933fce161
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-19 21:13:10 +0530

    Compare two numbers

---
+++ Compare two numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // code here
+    int i;
+    int j;
+    cin>>i;
+    cin>>j;
+    
+    if(i<=j){
+        cout<<"0";
+    }
+    else{
+        cout<<"1";
+    }
+
+    return 0;
+}
