88cbe81f3927fc43a5bff07e0213487e57d4b76b
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-20 20:49:09 +0530

    Sum of N Numbers

--- Sum of N Numbers.cpp
+++ Sum of N Numbers.cpp
@@ -9,3 +9,25 @@ int nSum(int n) {
     }
     return ans;
 }
+
+
+
+//alternate solution of alternate problem but logic is same
+
+/*
+
+
+int nSum(int n) {
+    // code here
+    
+    int ans = 0;
+    
+    for(int i=1;i<=n;i++){
+        ans+=i;
+    }
+    
+    return ans;
+}
+
+
+*/
