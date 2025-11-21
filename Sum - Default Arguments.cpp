986431fb1db9c81c17c95f78c03736f5a7f35e58
Shivam Maurya <shivam2024maurya@gmail.com>
2025-11-22 00:18:51 +0530

    Sum - Default Arguments

---
+++ Sum - Default Arguments.cpp
@@ -0,0 +1,39 @@
+int sum( int a,int b,int c=0,int d=0) {
+    // code here
+    
+    return a+b+c+d;
+    
+}
+
+
+
+/*
+explanation:-
+
+a aur b required parameters hain - yani function ko kam se kam 2 values zaroor deni padegi.
+
+c aur d ke baad = 0 likha hua hai — iska matlab default value 0 set ki gayi hai.
+
+Agar caller c ya d na de to woh automatically 0 le liya jayega
+
+*/
+
+
+
+
+
+
+
+/*int main(){
+    int i, j ,k, l;
+    cin>>i;
+    cin>>j;
+    cin>>k;
+    cin>>l;
+    
+    
+    cout<<sum(i,j)<<endl;
+    cout<<sum(i,j,k)<<endl;
+    cout<<sum(i,j,k,l)<<endl;
+    
+}*/
